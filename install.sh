a731a182b8b5fa72028f887546bf553cec73b429
boris <boris.px1@gmail.com>
2019-08-09 20:37:14 +0100

    Added google enxtention with bundle

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
+CURRENT_PATH=$PWD
+
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # copy all configuration
 cp vimrc ~/.vimrc
 
-# install everything
-vim +PluginInstall +qall
-
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install all the bundles
+cd ~/.vim/bundle
+# vim-g
+git clone https://github.com/szw/vim-g.PluginInstall
+
+# install everything
+vim +PluginInstall +qall
+
+
+cd $CURRENT_PATH
+
